c489305071083b656c9c5669cb1f424a644ece75
Eugen3113 <eg31it@gmail.com>
2025-08-01 19:39:42 +0200

    add lesson 03

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 // Lessons
 import Lesson_02 from "./lessons/Lesson_02/Lesson_02";
+import Lesson_03 from "./lessons/lesson_03/Lesson_03";
 
 // Homeworks
 import Homework_02 from "./homeworks/Homework_02/Homework_02";
 
 import "./App.css";
 
-
 function App() {
   return (
     <div className="app">
       {/* Lessons */}
-      <Lesson_02 />
+      {/* <Lesson_02 /> */}
+      <Lesson_03 />
       {/* Homeworks */}
-      <Homework_02 />
-      <Homework_02 />
-      <Homework_02 />
-
+      {/* <Homework_02 /> */}
     </div>
   );
 }
--- src/homeworks/Homework_02/Homework_02.jsx
+++ src/homeworks/Homework_02/Homework_02.jsx
@@ -1,25 +1,15 @@
-import Card from "../../components/Card/Card";
+import SimpsonsCard from "../../components/SimpsonsCard/SimpsonsCard";
+
 import "./styles.css";
 
 function Homework_02() {
-
-  const human = {
-  fullName: "Humor Simpson",
-  typeOfActiviti: "Chemist and safety inspector",
-  hobby: "Eating donuts",
-  avatarURL:
-    "https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png",
-};
-
-    return(
-         <div className="card">
-        <img className="avatar_pic" src={human.avatarURL} alt="Human image"/>
-        <h3 className="card_title_person">Person card: {human.fullName}</h3>
-        <p className="card_info">Type of Activiti: {human.typeOfActiviti}</p>
-        <p className="card_info">Hobby: {human.hobby}</p>
-        </div>
-      
-    );
+  return (
+    <div className="homework02_page_wrapper">
+      <SimpsonsCard />
+      <SimpsonsCard />
+      <SimpsonsCard />
+    </div>
+  );
 }
 
 export default Homework_02;
