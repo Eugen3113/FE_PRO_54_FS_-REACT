91c378f201f61126a69599aaa2877274b177c4af
Eugen3113 <eg31it@gmail.com>
2025-08-07 18:19:25 +0200

    add lesson 05

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import Lesson_02 from "./lessons/Lesson_02/Lesson_02";
 import Lesson_03 from "./lessons/lesson_03/Lesson_03";
 import Lesson_04 from "./lessons/lesson_04/Lesson_04";
+import Lesson_05 from "./lessons/lesson_05/Lesson_05";
 
 // Homeworks
 import Homework_02 from "./homeworks/Homework_02/Homework_02";
@@ -25,11 +26,12 @@ function App() {
       {/* <Lesson_02 /> */}
       {/* <Lesson_03 /> */}
       {/* <Lesson_04 /> */}
+      <Lesson_05 />
 
       {/* Homeworks */}
       {/* <Homework_02 /> */}
       {/* <Homework_03/> */}
-      <Homework_04/>
+      {/* <Homework_04/> */}
       
 
 
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,34 +1,50 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Button from '../Button/Button';
+import Button from "../Button/Button";
 
-import './styles.css';
+import "./styles.css";
 
 function Feedback() {
+  const [like, setLike] = useState(0); // Если мы ничего не передаем в качестве аргумента функции useState(), то 1 элемент массива, который эта функция возвращает === undefined
+  const [dislike, setDislike] = useState(0);
 
-  const [likes, setLikes] = useState(0);
-  const [dislikes, setDislikes] = useState(0);
-
-
-  const handleLike = () => setLikes(likes + 1);
-  const handleDislike = () => setDislikes(dislikes + 1);
-  const handleReset = () => {
-
-    setLikes(0);
-     setDislikes(0);
+  const onLike = () => {
+    setLike((prevValue) => {
+      console.log(prevValue);
+      return prevValue + 1;
+    });
+    setLike((prevValue) => {
+      console.log(prevValue);
+      return prevValue + 1;
+    });
+    setLike((prevValue) => {
+      console.log(prevValue);
+      return prevValue + 1;
+    });
   };
 
+  const onDislike = () => {
+    setDislike((prevValue) => prevValue + 1);
+  };
 
+  const resetResults = () => {
+    setLike(0);
+    setDislike(0);
+  };
 
   return (
-    <div className="feedback-container">
-      <Button onClick={handleLike}>Like</Button>
-      <span>{likes}</span>
-      
-      <Button onClick={handleDislike}>Dislike</Button>
-      <span>{dislikes}</span>
-      
-      <Button onClick={handleReset}>Reset Results</Button>
+    <div className="feedback-wrapper">
+      <div className="feedback-control">
+        <div className="buttonwithcount-container">
+          <Button name="Like" onClick={onLike} />
+          <p className="count">{like}</p>
+        </div>
+        <div className="buttonwithcount-container">
+          <Button name="Dislike" onClick={onDislike} />
+          <p className="count">{dislike}</p>
+        </div>
+      </div>
+      <Button name="Reset Results" onClick={resetResults} />
     </div>
   );
 }
